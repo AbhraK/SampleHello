d41dd8edcbbd89f571d252ef0847bfed422d5c4f
AbhrajyotiK <rajkundu91@gmail.com>
2018-11-08 23:44:15 +0530

    Test code updated

---
+++ SampleHelloWorld/SampleTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleHello;
+
+
+namespace SampleTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("hello world", Program.CreateMessage());
+        }
+    }
+}
