d2faff5147a617b547d4685b70f8d15f94a0cb21
Abhrajyoti Kundu <rajkundu91@gmail.com>
2018-11-08 14:56:10 +0530

    Hello

---
+++ SampleHelloWorld/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleHello;
+
+namespace SampleHelloTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           Assert.AreEqual("hello world",Program.CreateMessage());
+        }
+    }
+}
